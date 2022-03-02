714eb2a67cf221b2d876fa2ebe90ccd38bc11d76
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-03-01 20:34:12 -0700

    activate banner

--- site/source/conf.py
+++ site/source/conf.py
@@ -94,9 +94,9 @@ html_theme_path = ["../../sphinx-scylladb-theme"]
 html_theme_options = {
     "conf_py_path": "site/source/",
     "banner_button_text": "Learn more",
-    "banner_button_url": "https://start9labs.medium.com/embassyos-0-3-0-f3d2d2ea016f",
+    "banner_button_url": "https://start9labs.medium.com/announcing-embassyos-0-3-0-f3d2d2ea016f",
     "banner_title_text": "EmbassyOS 0.3.0 is now available 🥳",
-    "hide_banner": "true",
+    "hide_banner": "false",
     "hide_edit_this_page_button": "false",
     "hide_sidebar_index": "true",
     "hide_version_dropdown": [],
