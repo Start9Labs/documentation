024a2ecf88c02518bc34052a72fce00234a8152d
Lucy Cifferello <12953208+elvece@users.noreply.github.com>
2022-06-15 16:05:04 -0600

    update deploy script

--- site/_utils/deploy_staging.sh
+++ site/_utils/deploy_staging.sh
@@ -13,7 +13,7 @@ echo "FILTER: make multiversion"
 make clean && make multiversion
 
 echo "FILTER: rsync build"
-rsync -rzP --delete ./_build/ root@staging.start9labs.com:/var/www/html/staging.start9labs.com
+rsync -rzP --delete ./_build/dirhtml root@staging.start9labs.com:/var/www/html/staging.start9labs.com/dirhtml
 
 echo "FILTER: ssh restart nginx and tor"
 ssh root@staging.start9labs.com "systemctl reload nginx && systemctl reload tor"
